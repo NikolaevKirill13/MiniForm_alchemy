573e18307daf0e9f393e631815f8baa6335cd188
st <storm_ne@inbox.ru>
2025-04-22 16:45:20 +0300

    add docstring to utils

--- miniform/utils.py
+++ miniform/utils.py
@@ -1,11 +1,31 @@
+from typing import Union
+
 import bcrypt
 
-from sqlalchemy import inspect
 from sqlalchemy.orm import DeclarativeBase
 
 
 def get_class_name_with_table_name(name: str):
+    """
+    Возвращает класс по имени таблицы.
+
+    Args:
+        name: имя таблицы
+
+    Returns:
+        класс
+    """
+
     def find_class(cls):
+        """
+        Ищет класс по имени таблицы.
+
+        Args:
+            cls:
+
+        Returns:
+
+        """
         if hasattr(cls, "__tablename__") and cls.__tablename__ == name:
             return cls
         for sub_cls in cls.__subclasses__():
@@ -20,17 +40,36 @@ def get_class_name_with_table_name(name: str):
     return result
 
 
-def hashed_func(value):
+def hashed_func(value: Union[str, int, float]):
+    """
+    Функция для шифрования паролей.
+
+    Args:
+        value: значение для шифрования
+
+    Returns:
+        str
+    """
     password = str(value).encode("utf-8")
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password, salt)
     return hashed_password.decode("utf-8")
 
 
-def check_hash(input_password, hashed_password):
+def check_hash(input_password: str, hashed_password: str) -> bool:
+    """
+    Функция проверки пароля
+
+    Args:
+        input_password: пароль для проверки.
+        hashed_password: валидное значение из базы данных.
+
+    Returns:
+        bool
+    """
     input_password = str(input_password).encode("utf-8")
     hashed_password = hashed_password.encode("utf-8")
     return bcrypt.checkpw(input_password, hashed_password)
 
 
-__all__ = ('hashed_func', 'check_hash')
+__all__ = ('hashed_func', 'check_hash', 'get_class_name_with_table_name')
